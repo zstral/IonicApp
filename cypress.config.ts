978adfc0f73ca3d7f1e044245adfb38cefbc49a6
Zstral <rafaelfernandezgalleguillos@outlook.com>
2025-07-16 21:52:26 -0400

    Changes in the UI (certs, work-exp, my-profile).

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   viewportWidth: 375,
+  viewportHeight: 800,
   e2e: {
     baseUrl: "http://localhost:8100",
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}",
